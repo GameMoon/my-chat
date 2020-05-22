7001152eee228a4510ffbc0bca8b685d56b672ae
udaniel <ugridaniel@gmail.com>
2020-05-22 13:46:26 +0200

    build added

---
+++ build/precache-manifest.f1791a850e7b23dee235de421c99b158.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c47ae9d9b1edf1b3f56950fdf60b599",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7b709c1433528175f0c3",
+    "url": "./static/css/main.92de519c.chunk.css"
+  },
+  {
+    "revision": "07c155597dc075058377",
+    "url": "./static/js/2.821ad3b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.821ad3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b709c1433528175f0c3",
+    "url": "./static/js/main.33f54396.chunk.js"
+  },
+  {
+    "revision": "763f551d24bf50b1938c",
+    "url": "./static/js/runtime-main.c7b6dbd4.js"
+  }
+]);
